e234e34de32e8e808c39fca51238f065beb4dac9
kivva33 <kenlil@bk.ru>
2023-04-27 17:19:38 +0300

    not commented code

--- main.cpp
+++ main.cpp
@@ -2,38 +2,22 @@
 #include "file.h"
 
 int main() {
-    /*LinkedList Nech;
-    LinkedList Chet;
-    Nech.push_back(11);
-    Nech.push_back(10);
-    Nech.push_back(9);
-    Nech.push_back(8);
-    Nech.push_back(7);
-
-    Chet.push_back(6);
-    Chet.push_back(5);
-    Chet.push_back(4);
-    Chet.push_back(3);
-    Chet.push_back(2);
-
-
-    print(&Chet);
-    printf("\n");
-    print(&Nech);*/
-#pragma region
     LinkedList test;
     test.push_back(3);
     test.push_back(4);
     test.push_back(5);
 
     print(&test);
+    std::cout << std::endl;
+
     int b;
     b = test[1];
     std::cout << b << std::endl;
     test[6] = 10;
-    test[5] = 11;
+    test[2] = 11;
 
     print(&test);
+    std::cout << std::endl;
     std::cout<< std::endl;
 
     LinkedList test1;
@@ -47,9 +31,20 @@ int main() {
     test2.push_back(5);
 
     if(test2==test1){
-        std::cout << "OK";
+        std::cout << "OK\n";
     }
-#pragma endregion
+    else{
+        std::cout << "Not equal\n";
+    }
+    test2.push_back(6);
+
+    if(test2==test1){
+        std::cout << "OK\n";
+    }
+    else{
+        std::cout << "Not equal\n";
+    }
+
     std::cout << std:: endl;
 
     LinkedList l1;
